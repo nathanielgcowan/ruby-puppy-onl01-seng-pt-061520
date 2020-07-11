19ee0743f488428f96ec818be2b20efa8b6ebf02
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-11 01:32:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+ 
+  @@all = []
+ 
+  def initialize(dog)
+    @name = name
+    self.save
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    self.each do|dog|
+      puts dog.name
+    end
+  end
+  def name
+    @name = name
+  end
+end
